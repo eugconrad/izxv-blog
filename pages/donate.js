ff1a4b1d5a4c18d226cd3dee71fc1e4f213fd40e
Eugene <orion25rus@gmail.com>
2022-12-09 01:22:57 +0200

    Some updates

--- pages/donate.js
+++ pages/donate.js
@@ -2,19 +2,21 @@ import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
 
-export default function FourZeroFour() {
+export default function Donate() {
   return (
     <>
-      <PageSEO title={`404 - ${siteMetadata.author}`} description="404" />
+      <PageSEO title={`Донат - ${siteMetadata.author}`} description="Донат" />
       <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
         <div className="space-x-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:px-3 md:text-8xl md:leading-14">
-            404
+            Донат
           </h1>
         </div>
         <div className="mx-auto max-w-2xl">
           <div className="md:border-l-2 md:px-6">
-            <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">Страница не найдена</p>
+            <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
+              Пока что здесь ничего нет
+            </p>
           </div>
         </div>
       </div>
